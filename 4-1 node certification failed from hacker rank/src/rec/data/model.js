471dcec43b16292d8513341e34b36c7e2807d620
bryan ruiz <andyruiz2468@gmail.com>
2024-05-09 13:13:53 -0600

    working
    notyet

--- 4-1 node certification failed from hacker rank/src/rec/data/model.js
+++ 4-1 node certification failed from hacker rank/src/rec/data/model.js
@@ -6,24 +6,38 @@ const myjsonpath = path.resolve(import.meta.dirname, "../data/recipes.json");
 // console.log(myjsonpath);
 
 export const getData = async () => {
-    let data2; // Declarar variable fuera del bloque try-catch
-  
+
+
     try {
         const rawData = await fs.promises.readFile(myjsonpath, 'utf-8');
         const data2 = JSON.parse(rawData);
         return data2;
       } catch (err) {
-        console.error('Error al leer o analizar el archivo JSON:', err);
+        // console.error('Error al leer o analizar el archivo JSON:', err);
         return null; // Or another appropriate value for error handling
       }
   };
 
+// const pathfile = path.join(import.meta.dirname,"recipes.json");
+
+// // console.log("esta es la ruta", pathfile);
+// export const getData = () => {
+//   // console.log("entrando en get");
+//   const data = fs.readFile(pathfile, "utf-8", (err, dataa) => {
+//     // console.log("entrando en funcion");
+//     if (err) {
+//       // console.log("esta dnado err",err);
+//       return err
+//     }
+//     const data2 = JSON.parse(dataa);
+//     // console.log("valor esperado", dataa);
+//     return data2;
+//   })
+//   return data;
+// }
+
 
 // Ejecuta `getdata()` y maneja la promesa devuelta
-// getData().then((data) => {
-//     console.log('Datos leídos:', data);
-// }).catch((err) => {
-//     console.error('Error:', err);
-// });
-
+// getData();
+// 
 
--- 4-1 node certification failed from hacker rank/src/rec/recipes/recipes.js
+++ 4-1 node certification failed from hacker rank/src/rec/recipes/recipes.js
@@ -1,26 +1,43 @@
 
-import express from 'express';
-import {getData} from '../data/model.js';
+import express, { json } from 'express';
 const router = express.Router();
-
+import { getData } from '../data/model.js';
 const data2 = await getData();
-// console.log(data2);
 
 router
-.get("/",(req,res)=>{
-    // console.log("llegando");
-    res.status(400).send({data: "NOT_FOUND",})
-})
+    .get("/", (req, res) => {
+        // console.log("llegando");
+        res.status(400).send({ data: "NOT_FOUND", })
+    })
 
-// ids esta esperando un arreglo separado por ,
-.get("/:ids",(req,res)=>{
+    // ids esta esperando un arreglo separado por ,
+    .get("/:ids", (req, res) => {
 
-    const ids = req.params.ids;
-    let valoresAceptados = /^[0-9]+$/;
-    if(ids.match(valoresAceptados)){return res.status(400).send({status:"X",message:"NOT_FOUND"});}
-    // console.log("ids -->",ids);
+        const ids = req.params.ids;
+        console.log(ids);
+        let valoresAceptados = /^[0-9]+$/;
 
-})
+        const idsArray = ids.split(",");
+
+        if(idsArray){
+        
+            const algunoCumple = idsArray.some(id => valoresAceptados.test(id));
+            if(!algunoCumple){
+            return res.status(400).send({messages:"NOT_FOUND"});
+            }
+        }
+        
+
+        let ar1 = idsArray.filter(id => valoresAceptados.test(id));
+        let a = [];
+        data2.forEach(row => {
+            if (ar1.includes(String(row.id))) {
+                a.push(row);
+            }
+        });
+
+        res.status(200).json({ status: "OK", data: a })
+    })
 
 
 export default router;
