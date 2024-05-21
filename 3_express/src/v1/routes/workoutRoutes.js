b733942d7f794496fb6cfd5b1df1fad18e5309c3
bryan ruiz <andyruiz2468@gmail.com>
2024-05-21 13:29:42 -0600

    Im gonna restart
    My pc for updates I wanna save my changes

--- 3_express/src/v1/routes/workoutRoutes.js
+++ 3_express/src/v1/routes/workoutRoutes.js
@@ -1,16 +1,48 @@
 const express = require("express");
-const router = express.Router();
+const apicache = require("apicache");
+
 const workoutController = require("../../controllers/workoutController");
 const RecordController = require("../../controllers/RecordController");
 
+
+const router = express.Router();
+const cache = apicache.middleware;
+
+/**
+    * @openapi
+    * /api/v1/workouts:
+    *   get:
+    *     tags:
+    *       - Workouts
+    *     responses:
+    *       200:
+    *         description: OK
+    *         content:
+    *           application/json:
+    *             schema:
+    *               type: object
+    *               properties:
+    *                 status:
+    *                   type: string
+    *                   example: OK
+    *                 data:
+    *                   type: array 
+    *                   items: 
+    *                     type: object
+    */
 router
-.get("/",workoutController.getAllWorkouts)
-.get("/:workoutId",workoutController.getOneWorkout)
-// this is a new route for records
-.get("/:workoutId/records",RecordController.getRecordForWorkout)
-// 
-.post("/",workoutController.createNewWorkout)
-.patch("/",workoutController.updateWorkout)
-.delete("/:workoutId",workoutController.deleteOneWorkout)
+    .get("/", cache("2 minutes"), workoutController.getAllWorkouts)
+    .get("/:workoutId", workoutController.getOneWorkout)
+    // this is a new route for records
+    .get("/:workoutId/records", RecordController.getRecordForWorkout)
+    // 
+    .post("/", workoutController.createNewWorkout)
+    .patch("/", workoutController.updateWorkout)
+    .delete("/:workoutId", workoutController.deleteOneWorkout)
 
 module.exports = router;
+
+// how work the middleare in node js .get("/",  here   ,workoutController.getAllWorkouts)
+// well, the place of here is in where the func or middleare is gonna works ex
+// if you make a middleare you need to put on there as ,mymiddl,controoller
+// and the workflow is enter in every until be there in the end. (end everything with next)
