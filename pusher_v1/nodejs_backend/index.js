3f176033f293850a0dc8b75a48d481f943a2b068
bryan ruiz <andyruiz2468@gmail.com>
2024-06-11 17:00:11 -0600

    pusher project
    Pusher js notify

---
+++ pusher_v1/nodejs_backend/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors')
+const Pusher = require("pusher");
+const path = require('path');
+
+
+const pusher = new Pusher({
+  appId: "1629802",
+  key: "efc5c18caf9f89f8e460",
+  secret: "130d712852b5c4eef4a5",
+  cluster: "mt1",
+  useTLS: true
+});
+
+const app = express();
+
+
+app.use(cors()); 
+
+app.use(express.json())
+
+app.post('/api/messages', async ( req, res)=>{
+    await pusher.trigger("banana-666", "chat-event", {
+        username: req.body.username,
+        message: req.body.message
+      });
+
+      res.json([]);
+})
+
+//servir worker??
+app.get('/service-worker.js', (req, res) => {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.sendFile(path.resolve(__dirname, 'service-worker.js'));
+});
+// 
+
+console.log('listening to port 8000');
+app.listen(8000);
